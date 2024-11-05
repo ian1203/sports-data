6c35dd0b8293568d30f37dc4fdaa3e78af28727e
Rafael Chantres Garcia <81809918+rafachan3@users.noreply.github.com>
2024-11-05 13:24:39 -0600

    clean_stat_value integration and error handling
    - Added `clean_stat_value` function for improved data parsing from raw text, allowing for better extracting numeric values from strings.
    
    - Modified error handling to pinpoint the exact player causing data extraction issues

--- data_manager.py
+++ data_manager.py
@@ -1,2 +1,19 @@
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
+
+def clean_stat_value(raw_text, index, is_percentage=False):
+    """
+    Cleans and converts a statistical value from a text string.
+    
+    Parameters:
+    - raw_text (str): The full text from which to extract the value.
+    - index (int): The index of the word in the split text to extract.
+    
+    Returns:
+    - float: The cleaned and converted value.
+    """
+    # Extract the specific word based on the index and remove parentheses
+    value_str = raw_text.split()[index]
+
+    return value_str
+
--- player_scrapper.py
+++ player_scrapper.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from data_manager import clean_stat_value
 
 import time
 
@@ -76,80 +77,83 @@ class PlayerScraper:
             self.select_competition()
 
             player_name = self.driver.find_element(By.XPATH, "//h2[@class='Text cuNqBu']").text
-            print("Player's name:", player_name)
+            print(f"Scraping {player_name}")
 
             games = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[4]//div[1]//div[2]//div[1]").text
-            amount_games = games.split()[2]
+            amount_games = clean_stat_value(games, 2)
+            print(games)
             print("Amount of games played:", amount_games)
 
             minutes_played = self.driver.find_element(By.XPATH,
                                                  "//div[@class='Box kNZKNS']//div[4]//div[1]//div[2]//div[4]").text
-            amount_minutes_played = minutes_played.split()[3]
+            amount_minutes_played = clean_stat_value(minutes_played, 3)
             print("Amount of minutes played:", amount_minutes_played)
 
             goals_conceded_per_game = self.driver.find_element(By.XPATH,
                                                           "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[1]").text
-            amount_goals_conceded_per_game = goals_conceded_per_game.split()[4]
+            print(goals_conceded_per_game)
+            amount_goals_conceded_per_game = clean_stat_value(goals_conceded_per_game, 4)
             print("Amount of goals conceded per game:", amount_goals_conceded_per_game)
 
             penalties_saved = self.driver.find_element(By.XPATH,
                                                   "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[2]").text
-            amount_penalties_saved = penalties_saved.split()[2]
+            amount_penalties_saved = clean_stat_value(penalties_saved, 2)
             print("Amount of penalties saved:", amount_penalties_saved)
 
             saves_per_game = self.driver.find_element(By.XPATH,
                                                  "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[3]").text
-            amount_saves_per_game = saves_per_game.split()[3]
+            amount_saves_per_game = clean_stat_value(saves_per_game, 3)
             print("Amount of saves per game:", amount_saves_per_game)
 
             saves_per_game_percentage = self.driver.find_element(By.XPATH,
                                                             "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[3]").text
-            amount_saves_per_game_percentage = saves_per_game_percentage.split()[4]
+            amount_saves_per_game_percentage = clean_stat_value(saves_per_game_percentage, 4)
             print("Percentage of saves per game:", amount_saves_per_game_percentage)
 
             goals_conceded = self.driver.find_element(By.XPATH,
                                                  "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[5]").text
-            amount_goals_conceded = goals_conceded.split()[2]
+            amount_goals_conceded = clean_stat_value(goals_conceded, 2)
             print("Amount of total goals conceded:", amount_goals_conceded)
 
             total_saves = self.driver.find_element(By.XPATH,
                                               "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[8]").text
-            amount_total_saves = total_saves.split()[2]
+            amount_total_saves = clean_stat_value(total_saves, 2)
             print("Amount of total saves:", amount_total_saves)
 
             goals_prevented = self.driver.find_element(By.XPATH,
                                                   "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[8]").text
-            amount_goals_prevented = goals_prevented.split()[2]
+            amount_goals_prevented = clean_stat_value(goals_prevented, 2)
             print("Amount of goals prevented:", amount_goals_prevented)
 
             passes_completed = self.driver.find_element(By.XPATH,
                                                    "//div[@class='Box kNZKNS']//div[7]//div[1]//div[2]//div[6]").text
-            amount_passes_completed = passes_completed.split()[3]
+            amount_passes_completed = clean_stat_value(passes_completed, 3)
             print("Amount of passes completed:", amount_passes_completed)
 
             passes_completed_percentage = self.driver.find_element(By.XPATH,
                                                               "//div[@class='Box kNZKNS']//div[7]//div[1]//div[2]//div[6]").text
-            amount_passes_completed_percentage = passes_completed_percentage.split()[4]
+            amount_passes_completed_percentage = clean_stat_value(passes_completed_percentage, 4, is_percentage=True)
             print("Percentage of passes completed:", amount_passes_completed_percentage)
 
             clean_sheets = self.driver.find_element(By.XPATH,
                                                "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[1]").text
-            amount_clean_sheets = clean_sheets.split()[2]
+            amount_clean_sheets = clean_stat_value(clean_sheets, 2)
             print("Amount of clean sheets:", amount_clean_sheets)
 
             error_leading_to_shot = self.driver.find_element(By.XPATH,
                                                         "//div[@class='Box kNZKNS']//div[7]//div[1]//div[2]//div[8]").text
-            amount_error_leading_to_shot = error_leading_to_shot.split()[4]
+            amount_error_leading_to_shot = clean_stat_value(error_leading_to_shot, 4)
             print("Amount of erros leading to shot:", amount_error_leading_to_shot)
 
             error_leading_to_goal = self.driver.find_element(By.XPATH,
                                                         "//div[@class='Box kNZKNS']//div[7]//div[1]//div[2]//div[8]").text
-            amount_error_leading_to_goal = error_leading_to_goal.split()[4]
+            amount_error_leading_to_goal = clean_stat_value(error_leading_to_goal, 4)
             print("Amount of errors leading to goal:", amount_error_leading_to_goal)
+            
         except TimeoutException:
-            print("Error: el elemento no estuvo disponible a tiempo")
+            print("Error: Element took too long to load.")
         except Exception as e:
-            print(f"Error al extraer datos del jugador: {e}")
+            print(f"Error extracting {player_name}'s data: {e}")
 
     def scrape_field_player_data(self, player_link):
         """
@@ -167,23 +171,23 @@ class PlayerScraper:
             self.select_competition()
 
             player_name = self.driver.find_element(By.XPATH, "//h2[@class='Text cuNqBu']").text
-            print("Player's name:", player_name)
+            print(f"Scraping {player_name}")
 
             games = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[4]//div[1]//div[2]//div[1]").text
-            amount_games = games.split()[2]
+            amount_games = clean_stat_value(games, 2)
             print("Amount of games played:", amount_games)
 
             minutes_played = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[4]//div[1]//div[2]//div[4]").text
-            amount_minutes_played = minutes_played.split()[3]
+            amount_minutes_played = clean_stat_value(minutes_played, 3)
             print("Amount of minutes played:", amount_minutes_played)
 
             goals = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[1]").text
-            amount_goals = goals.split()[1]
+            amount_goals = clean_stat_value(goals, 1)
             print("Amount of goals:", amount_goals)
 
-            xG_element = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[2]")
-            if "Expected Goals (xG)" in xG_element.text:
-                amount_xG = xG_element.text.split()[3]
+            xG_element = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[2]").text
+            if "Expected Goals (xG)" in xG_element:
+                amount_xG = clean_stat_value(xG_element, 3)
                 print("Amount of xG:", amount_xG)
 
                 shots_per_game = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[5]").text
@@ -191,86 +195,86 @@ class PlayerScraper:
                 print("Amount of shots per game:", amount_shots_per_game)
 
                 shots_target_per_game = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[5]//div[1]//div[2]//div[6]").text
-                amount_shots_target_per_game = shots_target_per_game.split()[5]
+                amount_shots_target_per_game = clean_stat_value(shots_target_per_game, 5)
                 print("Amount of shots on target per game:", amount_shots_target_per_game)
 
             assists = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[6]//div[1]//div[2]//div[1]").text
-            amount_assists = assists.split()[1]
+            amount_assists = clean_stat_value(assists, 1)
             print("Amount of assists:", amount_assists)
 
             xA = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[6]//div[1]//div[2]//div[2]").text
-            amount_xA = xA.split()[3]
+            amount_xA = clean_stat_value(xA, 3)
             print("Amount of xA:", amount_xA)
 
             big_chances_created = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[6]//div[1]//div[2]//div[4]").text
-            amount_big_chances_created = big_chances_created.split()[3]
+            amount_big_chances_created = clean_stat_value(big_chances_created, 3)
             print("Amount of big chances created:", amount_big_chances_created)
 
             key_passes_per_game = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[6]//div[1]//div[2]//div[5]").text
-            amount_key_passes_per_game = key_passes_per_game.split()[2]
+            amount_key_passes_per_game = clean_stat_value(key_passes_per_game, 2)
             print("Amount of key passes per game:", amount_key_passes_per_game)
 
             passes_completed_per_game = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[6]//div[1]//div[2]//div[6]").text
-            amount_passes_completed_per_game = passes_completed_per_game.split()[3]
+            amount_passes_completed_per_game = clean_stat_value(passes_completed_per_game, 3)
             print("Amount of passes completed per game:", amount_passes_completed_per_game)
 
             passes_completed_per_game_percentage = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[6]//div[1]//div[2]//div[6]").text
-            amount_passes_completed_per_game_percentage = passes_completed_per_game_percentage.split()[4]
+            amount_passes_completed_per_game_percentage = clean_stat_value(passes_completed_per_game_percentage, 4)
             print("Pass completion percentage:", amount_passes_completed_per_game_percentage)
 
             succesful_dribles = self.driver.find_element(By.XPATH, "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[1]").text
-            amount_succesful_dribles = succesful_dribles.split()[2]
+            amount_succesful_dribles = clean_stat_value(succesful_dribles, 2)
             print("Successful dribbles per game:", amount_succesful_dribles)
 
             total_duels_won_per_game = self.driver.find_element(By.XPATH,
                                                         "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[2]").text
-            amount_total_duels_won_per_game = total_duels_won_per_game.split()[3]
+            amount_total_duels_won_per_game = clean_stat_value(total_duels_won_per_game, 3)
             print("Total duels won per game:", amount_total_duels_won_per_game)
 
             total_duels_won_percentage = self.driver.find_element(By.XPATH,
                                                             "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[2]").text
-            amount_total_duels_won_percentage = total_duels_won_percentage.split()[4]
+            amount_total_duels_won_percentage = clean_stat_value(total_duels_won_percentage, 4)
             print("Percentage of total duels won per game:", amount_total_duels_won_percentage)
 
             ground_duels_won_per_game = self.driver.find_element(By.XPATH,
                                                             "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[3]").text
-            amount_ground_duels_won_per_game = ground_duels_won_per_game.split()[3]
+            amount_ground_duels_won_per_game = clean_stat_value(ground_duels_won_per_game, 3)
             print("Ground duels won per game:", amount_ground_duels_won_per_game)
 
             ground_duels_won_per_game_percentage = self.driver.find_element(By.XPATH,
                                                                     "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[3]").text
-            amount_ground_duels_won_per_game_percentage = ground_duels_won_per_game_percentage.split()[4]
+            amount_ground_duels_won_per_game_percentage = clean_stat_value(ground_duels_won_per_game_percentage, 4)
             print("Percentage of ground duels won per game:", amount_ground_duels_won_per_game_percentage)
 
             aerial_duels_won_per_game = self.driver.find_element(By.XPATH,
                                                                 "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[4]").text
-            amount_aerial_duels_won_per_game = aerial_duels_won_per_game.split()[3]
+            amount_aerial_duels_won_per_game = clean_stat_value(aerial_duels_won_per_game, 3)
             print("Aerial duels won per game:", amount_aerial_duels_won_per_game)
 
             aerial_duels_won_per_game_percentage = self.driver.find_element(By.XPATH,
                                                                         "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[4]").text
-            amount_aerial_duels_won_per_game_percentage = aerial_duels_won_per_game_percentage.split()[4]
+            amount_aerial_duels_won_per_game_percentage = clean_stat_value(aerial_duels_won_per_game_percentage, 4)
             print("Percentage of aerial duels won per game:", amount_aerial_duels_won_per_game_percentage)
 
             possession_lost_per_game = self.driver.find_element(By.XPATH,
                                                             "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[5]").text
-            amount_possession_lost_per_game = possession_lost_per_game.split()[2]
+            amount_possession_lost_per_game = clean_stat_value(possession_lost_per_game, 2)
             print("Possession lost per game:", amount_possession_lost_per_game)
 
             fouls_received_per_game = self.driver.find_element(By.XPATH,
                                                             "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[7]").text
-            amount_fouls_received_per_game = fouls_received_per_game.split()[2]
+            amount_fouls_received_per_game = clean_stat_value(fouls_received_per_game, 2)
             print("Fouls received per game:", amount_fouls_received_per_game)
 
             offside_per_game = self.driver.find_element(By.XPATH,
                                                     "//div[@class='Box kNZKNS']//div[8]//div[1]//div[2]//div[8]").text
-            amount_offside_per_game = offside_per_game.split()[1]
+            amount_offside_per_game = clean_stat_value(offside_per_game, 1)
             print("Offsides per game:", amount_offside_per_game)
 
         except TimeoutException:
             print("Error: Element took too long to load.")
         except Exception as e:
-            print(f"Error extracting player data: {e}")
+            print(f"Error extracting {player_name}'s data: {e}")
 
     def scrape_players_data(self):
         """
